a10ba0dab3bc7e16a8828d89bb0a62af043073bc
Supreet Kumar <supreet7111999@gmail.com>
2023-02-18 01:40:50 +0530

    Updates server.js
    Added 4 post api for change pass , register(signup) , update and login

--- Server/server.js
+++ Server/server.js
@@ -3,19 +3,28 @@ require("./db/conn");
 const port=process.env.PORT||3000;
 const User=require("./models/user");
 const app=express();
-const bodyParser=require("body-parser");
 
-app.use(bodyParser.urlencoded({extended:false, limit:'50mb'}));
+
+app.use(express.json());
 app.get("/",(_,res)=>{
     res.send("Welcome");
 })
 app.post("/register",async (req,res)=>{
      try{
         console.log(req.body);
-       res.send(req.body);
+            
      }catch(err){
         res.status(500).send(err);
      }
+})
+app.post("/login",async (req,res)=>{
+
+})
+app.post("/update",(req,res)=>{
+
+})
+app.post("/changepass",(req,res)=>{
+
 })
 app.listen(port,()=>{
     console.log(`server is running ${port}`);
