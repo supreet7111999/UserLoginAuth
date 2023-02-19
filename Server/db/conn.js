32ec7ba6c372857db77dedc01eddfe001a07b31b
Supreet Kumar <supreet7111999@gmail.com>
2023-02-19 23:16:51 +0530

    connecting to the mongodb atlas

--- Server/db/conn.js
+++ Server/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 // const url="mongodb+srv://root:root@cluster0.vcvx4oj.mongodb.net/?retryWrites=true&w=majority";
-const url="mongodb://127.0.0.1:27017/user";
+const url="mongodb+srv://root:root@cluster0.kzjvrgp.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(url).then(()=>{
     console.log("Database connected");
 }).catch((err)=>{
